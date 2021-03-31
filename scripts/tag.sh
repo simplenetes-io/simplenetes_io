3804910503bca6a687d21e78b167ca6773e9ed34
Thomas Backlund <blund@blockie.org>
2021-03-31 13:01:05 +0200

    Update ./scripts/tag.sh so it requires a clean repo

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+test -z "$(git status --porcelain)" || {
+    printf "Git repo not comitted, cannot tag.\\n" >&2
+    exit 1;
+}
+
 if ! podVersion="$(./scripts/version.sh get)"; then
     exit 1
 fi
